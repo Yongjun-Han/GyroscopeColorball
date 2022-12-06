eed3eab3ecdde624ce1fcbd7a470997f6fbaef57
yongjunhan <hanyongjun.dev@gmail.com>
2022-12-06 17:47:56 +0900

    Initial Commit

---
+++ GyroscopeColorball/GyroscopeColorballApp.swift
@@ -0,0 +1,17 @@
+//
+//  GyroscopeColorballApp.swift
+//  GyroscopeColorball
+//
+//  Created by YongJun Han on 2022/12/06.
+//
+
+import SwiftUI
+
+@main
+struct GyroscopeColorballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
